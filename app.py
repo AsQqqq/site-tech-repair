a10a8039fc3beff8d05875a6ae4421830e2dc862
AsQqqq <asqdanil@yandex.ru>
2025-01-09 00:43:48 +0300

    Configure Flask app to run with SSL on port 443

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def main_url():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=443, ssl_context=('/etc/letsencrypt/live/repair-31.ru/fullchain.pem', '/etc/letsencrypt/live/repair-31.ru/privkey.pem'))
