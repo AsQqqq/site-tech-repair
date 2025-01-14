4ab46485d2da206edc8e87a02c0f2d0bb935b0ad
AsQqqq <asqdanil@yandex.ru>
2025-01-15 00:13:58 +0300

    Update phone number for mobile contact functionality in index.js

--- app/static/script/index.js
+++ app/static/script/index.js
@@ -1,7 +1,8 @@
 function contactUs() {
     if (window.innerWidth <= 768) {
         // Для мобильных устройств инициируем звонок
-        window.location.href = "tel:8915222006";
+        window.location.href = "tel:89155222006";
+        
     } else {
         // Для ПК показываем номер телефона в модальном окне
         document.getElementById('modal').classList.add('show');
