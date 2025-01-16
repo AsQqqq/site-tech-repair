32168985c4b44c31b8927a967e48b98479d7b07a
AsQqqq <asqdanil@yandex.ru>
2025-01-16 23:41:21 +0300

    Add Telegram notification for new contract applications and update config for API credentials

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ class Config:
     'sqlite:///' + os.path.join(basedir, 'app.db')
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or 'app/static/uploads/photos'
     WEEKLY_FOLDER = os.environ.get('WEEKLY_FOLDER') or 'app/static/uploads/weekly'
+    TOKEN = os.environ.get('TOKEN') or '7792011510:AAHK_rKdfaMHKWQX9o7DW8jVsqxFnGToYQU'
+    GROUP_ID = os.environ.get('GROUP_ID') or '-4614020839'
