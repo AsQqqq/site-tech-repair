7dc7399efa52b32c3a4926303320eae76d2570dc
AsQqqq <asqdanil@yandex.ru>
2025-01-17 06:22:14 +0300

    Update TOKEN in config to remove trailing underscores

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     'sqlite:///' + os.path.join(basedir, 'app.db')
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or 'app/static/uploads/photos'
     WEEKLY_FOLDER = os.environ.get('WEEKLY_FOLDER') or 'app/static/uploads/weekly'
-    TOKEN = os.environ.get('TOKEN') or '7792011510:AAHK_rKdfaMHKWQX9o7DW8jVsqxFnGToYQU___'
+    TOKEN = os.environ.get('TOKEN') or '7792011510:AAHK_rKdfaMHKWQX9o7DW8jVsqxFnGToYQU'
     GROUP_ID = os.environ.get('GROUP_ID') or '-4614020839'
     API_VERSION = os.environ.get('API_VERSION') or 'v1'
     LIMIT_API_KEY = os.environ.get('LIMIT_API_KEY') or '1'
